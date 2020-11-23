63f27fa6b3ad375f4b1cd9a2fb454cdd3c4039e8
Rob Sykes <rob@sykestech.net>
2020-11-23 04:27:44 +0000

    More clean up and better info on the webpage.

--- src/maze.ts
+++ src/maze.ts
@@ -1,6 +1,9 @@
 import {HEIGHT, WIDTH} from "./constants";
 import Cell from "./cell";
 
+/**
+ * Implementation of a Maze.
+ */
 export default class Maze {
     public _width: number = 0;
     public _height: number = 0;
@@ -17,7 +20,63 @@ export default class Maze {
         this._cellHeight = HEIGHT / this._height;
     }
 
-    generate(width: number, height: number) {
+    private getNeighbours(cell: Cell, cells: Cell[][], width: number, height: number) {
+        let cellX: number = cell._x;
+        let cellY: number = cell._y;
+        let neighbours: Cell[] = [];
+        let mazeCells = cells;
+
+        if (cellX > 0) { // Left
+            neighbours.push(mazeCells[cellX - 1][cellY]);
+        }
+
+        if ( cellX < (width - 1)) { // Right
+            neighbours.push(mazeCells[cellX + 1][cellY]);
+        }
+
+        if (cellY > 0) { // Top
+            neighbours.push(mazeCells[cellX][cellY - 1]);
+        }
+
+        if (cellY < (height - 1)) { // Bottom
+            neighbours.push(mazeCells[cellX][cellY + 1]);
+        }
+
+        return neighbours;
+    }
+
+    private breakWalls(currentCell: Cell, neighbourCell: Cell) {
+        if (currentCell._x > neighbourCell._x) { // From Left
+            currentCell._left = false;
+            neighbourCell._right = false;
+        }
+
+        if (currentCell._x < neighbourCell._x) { // From Right
+            currentCell._right = false;
+            neighbourCell._left = false;
+        }
+
+        if (currentCell._y > neighbourCell._y) { // From Bottom
+            currentCell._top = false;
+            neighbourCell._bottom = false;
+        }
+
+        if (currentCell._y < neighbourCell._y) { // From Top
+            currentCell._bottom = false;
+            neighbourCell._top = false;
+        }
+    }
+
+    /**
+     * This generate implements the DFS (Depth First Search) algorithm,
+     * in order to generate the maze.
+     *
+     * https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_depth-first_search
+     *
+     * @param width
+     * @param height
+     */
+    public generate(width: number, height: number) {
         this._width = width;
         this._height = height;
         this._cellWidth = WIDTH / this._width;
@@ -59,7 +118,7 @@ export default class Maze {
         }, 1, this._stack, this.getNeighbours, this.breakWalls, this._cells, this._width, this._height);
     }
 
-    clearMaze() {
+    public clearMaze() {
         this._done = true;
 
         for(let x = 0; x < this._width; x++) {
@@ -67,53 +126,6 @@ export default class Maze {
             for(let y = 0; y < this._height; y++) {
                 this._cells[x][y] = new Cell(x, y, this._cellWidth, this._cellHeight);
             }
-        }
-    }
-
-    getNeighbours(cell: Cell, cells: Cell[][], width: number, height: number) {
-        let cellX: number = cell._x;
-        let cellY: number = cell._y;
-        let neighbours: Cell[] = [];
-        let mazeCells = cells;
-
-        if (cellX > 0) { // Left
-            neighbours.push(mazeCells[cellX - 1][cellY]);
-        }
-
-        if ( cellX < (width - 1)) { // Right
-            neighbours.push(mazeCells[cellX + 1][cellY]);
-        }
-
-        if (cellY > 0) { // Top
-            neighbours.push(mazeCells[cellX][cellY - 1]);
-        }
-
-        if (cellY < (height - 1)) { // Bottom
-            neighbours.push(mazeCells[cellX][cellY + 1]);
-        }
-
-        return neighbours;
-    }
-
-    breakWalls(currentCell: Cell, neighbourCell: Cell) {
-        if (currentCell._x > neighbourCell._x) { // From Left
-            currentCell._left = false;
-            neighbourCell._right = false;
-        }
-
-        if (currentCell._x < neighbourCell._x) { // From Right
-            currentCell._right = false;
-            neighbourCell._left = false;
-        }
-
-        if (currentCell._y > neighbourCell._y) { // From Bottom
-            currentCell._top = false;
-            neighbourCell._bottom = false;
-        }
-
-        if (currentCell._y < neighbourCell._y) { // From Top
-            currentCell._bottom = false;
-            neighbourCell._top = false;
         }
     }
 }
